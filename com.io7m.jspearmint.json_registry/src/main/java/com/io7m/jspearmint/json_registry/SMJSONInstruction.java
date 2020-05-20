94264d2cf27122e9e1302a3b6daea6065be3a20d
Mark Raynsford <code@io7m.com>
2020-05-20 11:57:15 +0000

    Initial version

---
+++ com.io7m.jspearmint.json_registry/src/main/java/com/io7m/jspearmint/json_registry/SMJSONInstruction.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright © 2020 Mark Raynsford <code@io7m.com> http://io7m.com
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR
+ * IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+package com.io7m.jspearmint.json_registry;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
+import java.math.BigInteger;
+import java.util.List;
+
+/**
+ * An instruction definition.
+ */
+
+// CHECKSTYLE:OFF
+
+@JsonDeserialize
+public final class SMJSONInstruction
+{
+  @JsonProperty(
+    required = true,
+    value = "opname"
+  )
+  public String name;
+
+  @JsonProperty(
+    required = true,
+    value = "class"
+  )
+  public String className;
+
+  @JsonProperty(
+    required = true,
+    value = "opcode"
+  )
+  public BigInteger opcode = BigInteger.ZERO;
+
+  @JsonProperty(
+    required = true,
+    value = "operands"
+  )
+  public List<SMJSONInstructionOperand> operands = List.of();
+
+  @JsonProperty(
+    required = false,
+    value = "capabilities"
+  )
+  public List<String> capabilities = List.of();
+
+  @JsonProperty(
+    required = false,
+    value = "version"
+  )
+  public String version = "";
+
+  @JsonProperty(
+    required = false,
+    value = "lastVersion"
+  )
+  public String lastVersion = "";
+
+  @JsonProperty(
+    required = false,
+    value = "extensions"
+  )
+  public List<String> extensions = List.of();
+
+  public SMJSONInstruction()
+  {
+
+  }
+}
---
+++ com.io7m.jspearmint.tests/src/test/java/com/io7m/jspearmint/tests/SMJSONRegistryTest.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright © 2020 Mark Raynsford <code@io7m.com> http://io7m.com
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR
+ * IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+package com.io7m.jspearmint.tests;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.io7m.jspearmint.json_registry.SMJSONRegistry;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.nio.file.Path;
+
+import static com.io7m.jspearmint.tests.SMJSONTestDirectories.createTempDirectory;
+import static com.io7m.jspearmint.tests.SMJSONTestDirectories.resourceStreamOf;
+
+public final class SMJSONRegistryTest
+{
+  private Path directory;
+
+  @BeforeEach
+  public void setup()
+    throws IOException
+  {
+    this.directory = createTempDirectory();
+  }
+
+  /**
+   * The registry, when parsed, contains the expected data.
+   *
+   * @throws IOException On errors
+   */
+
+  @Test
+  public void registryContainsExpectedData()
+    throws IOException
+  {
+    try (var stream = resourceStreamOf(
+      SMJSONRegistryTest.class,
+      this.directory,
+      "spirv_headers/include/spirv/unified1/spirv.core.grammar.json")) {
+
+      final var mapper = new ObjectMapper();
+      final var registry = mapper.readValue(stream, SMJSONRegistry.class);
+
+      Assertions.assertEquals("0x07230203", registry.magicNumber);
+      Assertions.assertEquals(BigInteger.ONE, registry.majorVersion);
+      Assertions.assertEquals(BigInteger.valueOf(5L), registry.minorVersion);
+      Assertions.assertEquals(555, registry.instructions.size());
+      Assertions.assertEquals(26, registry.instructionClasses.size());
+      Assertions.assertEquals(47, registry.operandKinds.size());
+    }
+  }
+}
